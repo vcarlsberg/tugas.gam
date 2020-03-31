b8de16331534d69d7832f03a5e3d5b2bd3d504ba
vcarlsberg <v.carlsberg@gmail.com>
2020-03-31 12:39:48 +0700

    - Add process description

--- pemodelan_gam.R
+++ pemodelan_gam.R
@@ -3,11 +3,14 @@ library(gam)
 library(mgcv)
 library(plyr)
 library(lmtest)
+library(Metrics)
 
+#load data
 mobil<-read.csv("file.csv")
 summary(mobil)
 attach(mobil)
 
+#plotting sebaran data
 modelmobil<-plyr::count(mobil,'model')
 modelmobil<-modelmobil[order(-modelmobil$freq),]
 barplot(head(modelmobil$freq,n = 3),
@@ -20,30 +23,46 @@ barplot(head(modelmobil$freq,n = 3),
         )
 detach(mobil)
 
-mobil<-mobil %>% filter(
-  model %in% c("Jazz","Avanza","Kijang Innova")
-)
+#filtering dataset yang variannya Avanza, Jazz & Innova dan filter outlier yang mileage nya <10000000
+mobil<-mobil %>% filter(model %in% 
+                          c("Avanza","Jazz","Kijang Innova")
+                        )
+mobil<-mobil %>% filter(mileage < 1000000 )
+
 attach(mobil)
 
+#Uji linearitas
 resettest(harga ~ mileage , power=3, type="regressor")
 resettest(harga ~ tahun , power=3, type="regressor")
 resettest(harga ~ mesin , power=3, type="regressor")
 
+#plotting x,y
 plot(mileage,harga)
 plot(tahun,harga)
 plot(mesin,harga)
 
-model.gam<-gam(harga~mesin+s(mileage)+s(tahun)
-               +factor(variant)
-               )
+#pemodelan GAM
+model.gam<-gam(harga~s(tahun)+(mileage)+mesin+variant)
 summary(model.gam)
 
-mean(model.gam$residuals)
-summary(model.gam$residuals)
+#pemodelan LM
+model.lm<-lm(harga~mesin+(mileage)+(tahun)
+             +(variant))
+summary(model.lm)
+summary(model.lm$residuals)
 
+#residual analysis
+summary(model.gam$residuals)
 hist(model.gam$residuals) 
 qqnorm(model.gam$residuals) 
 qqline(model.gam$residuals)
 
 plot(model.gam$fitted.values,model.gam$residuals) 
 plot(c(1:length(mesin)),model.gam$residuals)
+
+#predict on Lamudi Dataset
+data.prediksi<-data.frame(mesin=1500,mileage=57000,tahun=2015,variant="Veloz")
+hasil.prediksi<-predict(model.gam,data.prediksi)
+hasil.prediksi
+
+mape(c(96000000,235000000,153000000,215000000),c(87079934,242554062,146106625,218184780))
